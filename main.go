7d3b9f8368ea251aae323a81f23b0b3c0a1eb326
Summer-lights <1067088037@qq.com>
2025-03-08 20:00:29 +0800

    feat: set log level to Info in production environment

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"github.com/scutrobotlab/rm-search/common"
 	"github.com/scutrobotlab/rm-search/server"
 	"github.com/scutrobotlab/rm-search/svc"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	if common.IsProd() {
+		logrus.SetLevel(logrus.InfoLevel)
+	}
+
 	c := svc.ReadConfig("etc/config.yaml")
 	svc.InitContext(c)
 
