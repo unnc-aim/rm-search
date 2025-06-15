41bc4b15d4115eb60caf8984a6fc60c950e901b3
Summer-lights <1067088037@qq.com>
2025-06-15 21:23:16 +0800

    feat: enhance job scheduling with dynamic job configuration and immediate execution option

--- job/common.go
+++ job/common.go
@@ -7,6 +7,13 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+type Item struct {
+	Name         string   // 任务名称
+	Spec         string   // Cron 表达式
+	Job          cron.Job // 任务
+	OnceWhenInit bool     // 是否在初始化时立即执行一次
+}
+
 type Base struct {
 	SvcCtx *svc.Context
 }
@@ -21,12 +28,25 @@ func (b Base) Start() {
 	c := cron.New()
 	indexer := index.NewIndexer(b.SvcCtx)
 
-	_, err := c.AddJob("@every 1m", IncrementalJob{
-		Base:    b,
-		Indexer: indexer,
-	})
-	if err != nil {
-		panic(err)
+	jobs := []Item{
+		{
+			Name:         "Incremental Build",
+			Spec:         "@every 1m",
+			Job:          IncrementalJob{Base: b, Indexer: indexer},
+			OnceWhenInit: true,
+		},
+	}
+
+	for _, item := range jobs {
+		if item.OnceWhenInit {
+			logrus.Infof("Async run job [%s] immediately", item.Name)
+			go item.Job.Run()
+		}
+		id, err := c.AddJob(item.Spec, item.Job)
+		if err != nil {
+			logrus.Fatalf("Add job [%s] error: %v", item.Name, err)
+		}
+		logrus.Infof("Add job [%s] with ID %d", item.Name, id)
 	}
 
 	logrus.Infof("Init %d cron jobs", len(c.Entries()))
